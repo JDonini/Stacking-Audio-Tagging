9454cc1868e8ebd98fe6e94d49eb326e08710c89
JDonini <julianodonini89@gmail.com>
2019-05-09 15:37:27 -0300

    implementacao e teste da metrica Hamming-Loss closes #3

--- src/metrics.py
+++ src/metrics.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 import numpy as np
 
 
-def hamming_loss(y_true, y_pred):
-    y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
-    return K.mean(K.sum(- y_true * K.log(y_pred) - (1 - y_true) * K.log(1 - y_pred), axis=1))
-
-
 def auc_roc(y_true, y_pred):
     auc_roc = tf.metrics.auc(y_true, y_pred)[1]
     K.get_session().run(tf.local_variables_initializer())
     return auc_roc
+
+
+def hamming_loss(y_true, y_pred):
+    y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+    return K.mean(K.sum(- y_true * K.log(y_pred) - (1 - y_true) * K.log(1 - y_pred), axis=1))
