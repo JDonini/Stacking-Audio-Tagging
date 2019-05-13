3c3043b0a3843a1d7bfe3ec087d4b37b09a815a0
JDonini <julianodonini89@gmail.com>
2019-05-13 18:11:52 -0300

    fix generate spectrogram

--- src/generate_spectrogram.py
+++ src/generate_spectrogram.py
@@ -1,10 +1,9 @@
-import multiprocessing
 from tqdm import tqdm
-import time
-import os
-import sys
+from joblib import Parallel, delayed
+import multiprocessing
 import numpy as np
-import matplotlib.style as ms
+import sys
+import os
 import pylab
 import librosa
 import librosa.display
@@ -13,7 +12,7 @@ from generate_structure import AUDIO, AUDIO_MFCC, AUDIO_MEL_SPECTROGRAM, \
  AUDIO_WAVEFORM, AUDIO_STFT_PERCUSSIVE, AUDIO_STFT_HARMONIC, AUDIO_CHROMAGRAM
 sys.path.append('database')
 from config_project import EXT_AUDIO, EXT_IMG, FIG_SIZE, N_MELS, SR, OFFSET, DURATION
-ms.use('seaborn-muted')
+
 
 pbar = tqdm(total=len(os.listdir(AUDIO)))
 
@@ -90,14 +89,13 @@ def update(*a):
 
 if __name__ == '__main__':
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-    for file in os.listdir(AUDIO):
-        audio_name, _ = file.split(EXT_AUDIO)
-        y, _ = librosa.load(AUDIO + file, offset=OFFSET, duration=DURATION)
-        pool.apply_async(create_chromagram, args=(y, audio_name), callback=update)
-        pool.apply_async(create_mel_spectrogram, args=(y, audio_name))
-        pool.apply_async(create_mfcc, args=(y, audio_name))
-        pool.apply_async(create_stft_harmonic, args=(y, audio_name))
-        pool.apply_async(create_stft_percussive, args=(y, audio_name))
-        pool.apply_async(create_waveform, args=(y, audio_name))
+    for full_dir, file in [[(os.path.join(root, f)), (f)] for root, dirs, files in os.walk(AUDIO) for f in files]:
+        y, _ = librosa.load(full_dir, offset=OFFSET, duration=DURATION)
+        pool.apply_async(create_chromagram, args=(y, file.split(EXT_AUDIO)[0]), callback=update)
+        pool.apply_async(create_mel_spectrogram, args=(y, file.split(EXT_AUDIO)[0]))
+        pool.apply_async(create_mfcc, args=(y, file.split(EXT_AUDIO)[0]))
+        pool.apply_async(create_stft_harmonic, args=(y, file.split(EXT_AUDIO)[0]))
+        pool.apply_async(create_stft_percussive, args=(y, file.split(EXT_AUDIO)[0]))
+        pool.apply_async(create_waveform, args=(y, file.split(EXT_AUDIO)[0]))
     pool.close()
     pool.join()
--- src/metrics.py
+++ src/metrics.py
@@ -22,3 +22,20 @@ def ranking_loss(y_true, y_pred, gamma=2.0, mp=2.5, mn=0.5):
         neg_score = tf.cond(tf.equal(top_indices[0], pos_label), lambda: top_values[1], lambda: top_values[0])
         return tf.log(1.0 + tf.exp(gamma * (mp - pos_score))) + tf.log(1.0 + tf.exp(gamma * (mn + neg_score)))
     return tf.map_fn(_loss_elem, tf.range(tf.shape(y_true)[0]), dtype=K.floatx())
+
+
+class SubsetAccuracy(Callback):
+    def on_train_begin(self, logs={}):
+        self.accuracy_score = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        score = np.asarray(self.model.predict(self.validation_data[0]))
+        predict = np.round(np.asarray(
+            self.model.predict(self.validation_data[0])))
+        targ = self.validation_data[1]
+
+        self.confusion.append(confusion_matrix(targ, predict))
+        self.precision.append(precision_score(targ, predict))
+        self.recall.append(recall_score(targ, predict))
+        self.f1s.append(f1_score(targ, predict))
+        self.kappa.append(cohen_kappa_score(targ, predict))
